8d807b5d3ca5eb462c8ae97015a28b17065eafaa
Unknown <mattawan.meeraksa@mail.kmutt.ac.th>
2018-08-14 23:00:06 +0700

    create app2.js to indicate data in exercisepage.html

---
+++ app2.js
@@ -0,0 +1,10 @@
+new Vue({
+    el:"#How-to-Exercise", //อ้างอิงid
+    data:{ //ข้อมูลที่จะนำมาแสดง
+        sentence: "This website will help you to improve to lose weight",
+        navbar1: "Exercise",
+        navbar2: "Food",
+        navbar3: "Body Measurement",
+    }
+})
+     
